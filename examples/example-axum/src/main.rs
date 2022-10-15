5be6efd844ba5b287da4848a1595d74eb8647fd9
tamasfe <me@tamasfe.dev>
2022-10-15 21:30:34 +0200

    chore(aide): axum example cleanup

--- examples/example-axum/src/main.rs
+++ examples/example-axum/src/main.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use aide::{
     axum::ApiRouter,
-    openapi::{Info, OpenApi, Tag},
+    openapi::{OpenApi, Tag},
     transform::TransformOpenApi,
 };
 use axum::{http::StatusCode, Extension};
@@ -37,24 +37,18 @@ async fn main() {
         .finish_api_with(&mut api, api_docs)
         .layer(Extension(Arc::new(api)));
 
+    println!("Example docs are accessible at http://127.0.0.1:3000/docs");
+
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-fn api_docs(mut api: TransformOpenApi) -> TransformOpenApi {
-    let inner = api.inner_mut();
-
-    inner.info = Info {
-        title: "Aide axum Open API".into(),
-        description: Some("An example Todo application".into()),
-        ..Default::default()
-    };
-
+fn api_docs(api: TransformOpenApi) -> TransformOpenApi {
     api.title("Aide axum Open API")
         .summary("An example Todo application")
-        .description(include_str!("api.md"))
+        .description(include_str!("README.md"))
         .tag(Tag {
             name: "todo".into(),
             description: Some("Todo Management".into()),
