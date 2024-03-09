caf9c7e103f923ac50b8f7fb5203eb4e00e8eeef
Travis <travis@marketcake.com>
2024-03-09 09:34:22 -0800

    config tweaks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://nuxt.com/docs/api/nuxt-config
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, viewport-fit=cover, user-scalable=no, shrink-to-fit=no",
+    },
+    rootId: "app",
+  },
+  debug: false,
   devtools: {
     enabled: true,
   },
@@ -11,6 +19,10 @@ export default defineNuxtConfig({
     "@nuxtjs/i18n",
     "@nuxtjs/tailwindcss",
   ],
+  ssr: true,
+  runtimeConfig: {
+    public: {},
+  },
   typescript: {
     strict: true,
     typeCheck: true,
