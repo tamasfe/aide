10f19e6ba7d696cc2238cd901eda21325ec612fb
Travis <travis@marketcake.com>
2024-04-10 07:20:48 -0700

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,10 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
 // https://eslint.nuxt.com/packages/module
-export default withNuxt();
+export default withNuxt(
+  // custom config
+).override("nuxt/vue/rules", {
+  rules: {
+    "vue/singleline-html-element-content-newline": "off",
+  },
+});
