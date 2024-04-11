de4d1e38867e4a7498298220103cb9a39d73551c
Vshpak3 <vadimshpak3@gmail.com>
2024-04-11 13:22:37 +0300

    Update HeroSectionSlider.vue

--- components/HeroSectionSlider.vue
+++ components/HeroSectionSlider.vue
@@ -1,16 +1,10 @@
 <template>
   <div class="flex justify-center w-full">
     <div class="relative flex w-full max-w-[1232px] h-[472px] overflow-y-hidden">
-      <div class="w-full carousel__wrapper scrollbar-hide overflow-hidden rounded">
-        <div class="carousel__item select-none w-full h-full">
-          <div class="carousel__item--bg w-full h-full"></div>
-        </div>
-        <div class="carousel__item select-none w-full h-full">
-          <div class="carousel__item--bg w-full h-full"></div>
-        </div>
-        <div class="carousel__item select-none w-full h-full">
-          <div class="carousel__item--bg w-full h-full"></div>
-        </div>
+      <div class="w-full carousel__wrapper snap-x snap-mandatory scrollbar-hide overflow-y-hidden overflow-x-scroll whitespace-nowrap scroll-smooth rounded">
+        <NuxtLink v-for="slide in props.slides" :to="slide.link" class="carousel__item inline-block select-none snap-start w-full h-full">
+          <div class="w-full h-full bg-contain bg-repeat" :style="{backgroundImage: `url(${slide.imageUrl})`}"></div>
+        </NuxtLink>
       </div>
 
       <button class="flex items-center absolute h-full bottom-0 left-0" @click="onPreviousSlide()">
@@ -21,29 +15,33 @@
       </button>
 
       <div class="flex gap-x-2 absolute w-full max-w-[240px] bottom-[39px] left-[50%] translate-x-[-50%]">
-        <div v-for="slide in maxSlides" class="carousel__selector flex-1 h-[2px] bg-[#FFE33A] opacity-40" :class="{ active: activeSlide === slide - 1}"></div>
+        <div v-for="slide in maxSlides" class="transition-opacity flex-1 h-[2px] bg-[#FFE33A] opacity-40" :class="{ 'opacity-100': activeSlide === slide - 1}"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+const props = defineProps<{
+  slides?: Array<any>
+}>()
+
 let activeSlide:Ref<number> = ref(0);
 let maxSlides:Ref<number> = ref(0);
-let slides:NodeListOf<Element>;
+let slideElements:NodeListOf<Element>;
 
 onMounted(() => {
-  slides = document.querySelectorAll('.carousel__item');
-  maxSlides.value = slides.length;
+  slideElements = document.querySelectorAll('.carousel__item');
+  maxSlides.value = slideElements.length;
 })
 
 const onNextSlide = () => {
-  activeSlide.value + 1 < slides.length ? activeSlide.value++ : activeSlide.value = 0
-  slides[activeSlide.value].scrollIntoView(false)
+  activeSlide.value + 1 < slideElements.length ? activeSlide.value++ : activeSlide.value = 0
+  slideElements[activeSlide.value].scrollIntoView(false)
 }
 const onPreviousSlide = () => {
   activeSlide.value - 1 >= 0 ? activeSlide.value-- : activeSlide.value = maxSlides.value - 1
-  slides[activeSlide.value].scrollIntoView(false)
+  slideElements[activeSlide.value].scrollIntoView(false)
 }
 </script>
 
@@ -59,28 +57,7 @@ const onPreviousSlide = () => {
 
 .carousel {
   &__wrapper {
-    scroll-snap-type: x mandatory;
     -webkit-overflow-scrolling: touch;
-    white-space: nowrap;
-    overflow-x: scroll;
-    scroll-behavior: smooth;
-
-    & > .carousel__item {
-      width: 100%;
-      display: inline-block;
-      scroll-snap-align: start;
-
-      & .carousel__item--bg {
-        background-image: url('/images/slider-bg.png');
-        background-size: contain;
-      }
-    }
-  }
-  &__selector {
-    transition: opacity .35s ease;
-    &.active {
-      opacity: 1;
-    }
   }
 }
 </style>
