e7ff6e966956d09de25cab1031614521ef2997a5
Tarik Mehinagić <tarik.mehinagic@hotellistat.de>
2024-06-17 21:52:36 +0200

    fix: drawer migration

--- components/base/Drawer.vue
+++ components/base/Drawer.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import {
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogOverlay,
-  DialogPortal,
-  DialogRoot,
+  Dialog,
+  DialogPanel,
   DialogTitle,
-} from "radix-vue";
+  DialogDescription,
+  TransitionRoot,
+  TransitionChild,
+} from "@headlessui/vue";
 
 const emit = defineEmits(["update:opened"]);
 
@@ -51,13 +50,6 @@ const positionClass = computed(() => {
   return "fixed right-0 top-0 bottom-0 h-full min-h-0 ";
 });
 
-const animationName = computed(() => {
-  if (props.position === "left") {
-    return "giro__modal-slide-left";
-  }
-  return "giro__modal-slide-right";
-});
-
 const size = computed(() => {
   if (props.size) {
     switch (props.size) {
@@ -94,70 +86,110 @@ const size = computed(() => {
   return "max-w-lg";
 });
 
+const closeModal = () => {
+  opened.value = false;
+};
+
 defineOptions({
   inheritAttrs: false,
 });
+
+const slideTransition = computed(() => ({
+  enter: `duration-150 ease-out`,
+  enterFrom:
+    props.position === "left"
+      ? "transform -translate-x-full"
+      : "transform translate-x-full",
+  etnerTo:
+    props.position === "left"
+      ? "transform translate-x-0"
+      : "transform translate-x-0",
+  leave: `duration-150 ease-in`,
+  leaveFrom:
+    props.position === "left"
+      ? "transform translate-x-0"
+      : "transform translate-x-0",
+  leaveTo:
+    props.position === "left"
+      ? "transform -translate-x-full"
+      : "transform translate-x-full",
+}));
 </script>
 
 <template>
-  <DialogRoot v-model:open="opened">
-    <DialogPortal>
-      <DialogOverlay
-        class="fixed top-0 left-0 right-0 bottom-0 bg-black/40 z-[13]"
-      />
-      <Transition :name="animationName">
-        <DialogContent
-          class="bg-emphasis/85 backdrop-blur-lg p-[32px] outline-none z-[100]"
-          v-bind="$attrs"
-          :class="[positionClass, size]"
+  <Teleport to="body">
+    <TransitionRoot
+      appear
+      :show="opened"
+      as="template"
+    >
+      <Dialog
+        v-model:open="opened"
+        as="div"
+        class="relative z-10"
+        @close="closeModal"
+      >
+        <TransitionChild
+          as="template"
+          enter="duration-350 ease-out"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="duration-350 ease-in"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
         >
-          <DialogTitle class="text-xl font-semibold pb-4">
-            <slot name="title" />
-          </DialogTitle>
-          <DialogDescription>
-            <slot name="description" />
-          </DialogDescription>
-          <slot />
-          <DialogClose
-            class="absolute top-[20px] right-[20px] inline-flex h-[25px] w-[25px] outline-none"
-            aria-label="Close"
+          <div class="fixed inset-0 bg-black/40 z-[0]" />
+        </TransitionChild>
+
+        <div :class="[positionClass, size]">
+          <TransitionChild
+            as="template"
+            v-bind="slideTransition"
           >
-            X
-          </DialogClose>
-        </DialogContent>
-      </Transition>
-    </DialogPortal>
-  </DialogRoot>
+            <DialogPanel
+              class="bg-emphasis/85 backdrop-blur-lg rounded-default p-6 flex flex-col gap-4 h-full"
+              v-bind="$attrs"
+            >
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6"
+              >
+                <div class="relative">
+                  <slot name="title" />
+                  <button
+                    type="button"
+                    class="absolute top-0 right-0 rounded-md text-emphasis hover:text-default"
+                    @click="closeModal"
+                  >
+                    <span class="sr-only">Close</span>
+                    <span class="text-2xl">X</span>
+                  </button>
+                </div>
+              </DialogTitle>
+              <DialogDescription>
+                <slot name="description" />
+              </DialogDescription>
+              <slot>
+                <div class="mt-2">
+                  <p class="text-sm text-gray-500">
+                    Your payment has been successfully submitted. We’ve sent you
+                    an email with all of the details of your order.
+                  </p>
+                </div>
+                <div class="mt-4">
+                  <button
+                    type="button"
+                    class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                    @click="closeModal"
+                  >
+                    Got it, thanks!
+                  </button>
+                </div>
+              </slot>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </Dialog>
+    </TransitionRoot>
+  </Teleport>
 </template>
-
-<style scoped>
-.giro__modal-slide-right-enter-active,
-.giro__modal-slide-left-enter-active {
-  transition: all 150ms ease-in-out;
-}
-
-.giro__modal-slide-right-leave-active,
-.giro__modal-slide-left-leave-active {
-  transition: all 150ms ease-in-out;
-}
-
-.giro__modal-slide-right-enter-from {
-  transform: translateX(100%);
-  opacity: 0;
-}
-
-.giro__modal-slide-right-leave-to {
-  transform: translateX(100%);
-  opacity: 0;
-}
-
-.giro__modal-slide-left-enter-from {
-  transform: translateX(-100%);
-  opacity: 0;
-}
-
-.giro__modal-slide-left-leave-to {
-  transform: translateX(-100%);
-  opacity: 0;
-}
-</style>
