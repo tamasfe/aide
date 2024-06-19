b5d630f0afa1ac686dd0c0ccadb315a42b777829
Tarik Mehinagić <tarik.mehinagic@hotellistat.de>
2024-06-19 22:21:12 +0200

    feat: register desktop

--- components/modal/Register.vue
+++ components/modal/Register.vue
@@ -15,31 +15,35 @@ const opened = computed({
   <BaseDialog
     v-bind="$attrs"
     v-model:opened="opened"
-    size="3xl"
+    size="6xl"
   >
-    <div class="flex flex-col gap-4 overflow-auto h-full">
-      <div class="relative w-full h-40">
+    <div class="flex flex-col sm:flex-row overflow-auto h-full">
+      <div class="flex-auto sm:flex-1 relative w-full sm:w-auto h-40 sm:h-auto">
         <div
-          class="absolute top-1/2 -translate-y-1/2 left-4 text-lg font-bold italic flex flex-col"
+          class="absolute top-1/2 sm:top-20 -translate-y-1/2 sm:translate-y-0 left-4 sm:left-1/2 sm:-translate-x-1/2 text-lg sm:text-4xl font-black italic leading-[3rem]"
         >
-          <span class="text-brand-yellow text-sm">EXCLUSIVE</span>
-          <span class="font-extrabold text-xl">PRE-RELEASE</span>
-          <span class="font-extrabold text-xl">FROM WILDJACK</span>
-          <span class="font-extrabold">
+          <p class="text-brand-yellow text-sm sm:text-2xl">EXCLUSIVE</p>
+          <p class="font-extrabold text-xl sm:text-5xl">PRE-RELEASE</p>
+          <p class="font-extrabold text-xl sm:text-5xl text-nowrap">
+            FROM WILDJACK
+          </p>
+          <p class="font-extrabold">
             <span class="text-brand-yellow">+100</span> FREE SPINS
-          </span>
+          </p>
         </div>
         <NuxtImg
           src="/assets/images/wheel.png"
           alt="Wheel"
-          class="object-cover w-full h-full"
+          class="object-cover w-full h-full sm:rounded-l-default"
         />
       </div>
-      <div class="inline-flex justify-center py-2">
-        <IconsLogo />
-      </div>
-      <div class="px-6">
-        <FormRegisterBrazil />
+      <div class="flex-auto sm:flex-1 flex flex-col gap-2 sm:gap-0 py-2">
+        <div class="inline-flex justify-center py-2 sm:pt-10">
+          <IconsLogo />
+        </div>
+        <div class="px-6 sm:py-6">
+          <FormRegisterBrazil />
+        </div>
       </div>
     </div>
   </BaseDialog>
