e88e61b39d25fdc1ecb0e20fd3c3cbbf6b2455ce
Travis <travis@marketcake.com>
2024-06-29 06:24:10 -0700

    fix: spell

--- components/HomeWinningNow.vue
+++ components/HomeWinningNow.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center">
-    <h3 class="text-2xl font-semibold min-w-[200px]">🏆 Winnig now:</h3>
+    <h3 class="text-2xl font-semibold min-w-[200px]">🏆 Winning now:</h3>
     <div
       class="flex-1 flex items-center gap-4 overflow-x-auto giro__hide-scroll"
     >
