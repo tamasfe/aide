6b325660ff4b49ef9e76c5bce83dbeec240f20ab
Tarik Mehinagić <tarik.mehinagic@hotellistat.de>
2024-08-16 19:08:58 +0200

    fix: types

--- types/api.d.ts
+++ types/api.d.ts
@@ -13,3 +13,22 @@ export type Metadata<T = Record<string, unknown>> = {
 export type ValidationErrorMetadata<T = Record<string, unknown>> = {
   [key: string]: Metadata<T>[];
 };
+
+export type GameImageVariant =
+  | "small"
+  | "medium"
+  | "large"
+  | "extra_large"
+  | "background";
+
+// temp until we generate the types which is getting fixed
+// as of moment I'm writing this
+export type Game = {
+  id: number;
+  ext_id: string;
+  name: string;
+};
+
+export type ApiData<T> = {
+  data: T;
+};
