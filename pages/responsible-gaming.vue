6b610c9d507f2f61ba1652e9219db2bebadc82e6
Travis <travis@marketcake.com>
2024-08-20 16:26:13 -0700

    feat: responsible gaming

--- pages/responsible-gaming.vue
+++ pages/responsible-gaming.vue
@@ -3,5 +3,58 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <h1>{{ t("page.responsible_gaming") }}</h1>
+  <div>
+    <WrapperPage
+      :title="t('page.responsible_gaming')"
+      version="1.0"
+      date="August 20, 2024"
+    >
+      <p>girobet.com is here to provide an excellent and enjoyable gaming experience and recognizes its responsibility in preventing problematic activity. Players are advised to take into account the following and not game irresponsibly:</p>
+
+      <ul>
+        <li>Play for entertainment, not to make money.</li>
+        <li>Avoid chasing losses.</li>
+        <li>Establish limits for yourself.</li>
+        <li>Do not let gambling interfere with your daily responsibilities.</li>
+        <li>Never gamble unless you can cover losses.</li>
+        <li>Take breaks.</li>
+      </ul>
+
+      <p>If you answer "YES" to the majority of the following questions, you are advised to take action to prevent gambling from negatively impacting your life:</p>
+      <ul>
+        <li>Does gambling affect your work?</li>
+        <li>Has gambling caused arguments with family/friends?</li>
+        <li>Do you always return to win back your losses?</li>
+        <li>Have you borrowed money to gamble?</li>
+        <li>Do you see gambling as a source of income?</li>
+        <li>Do you find it difficult to limit your gambling?</li>
+      </ul>
+
+      <h4>What to Do</h4>
+
+      <p>Listed below are reputable organizations committed to helping those who struggle with gambling problems, and can be contacted at any time:</p>
+      <ul>
+        <li>Gamblers Anonymous</li>
+        <li>Gambling Therapy</li>
+        <li>GamCare</li>
+      </ul>
+
+      <h4>How We Can Help</h4>
+
+      <p>We advise all players concerned about their gambling behavior to self-exclude from their gaming account. Self-exclusion will lock your account for a minimum of 6 months, and no promotional material will be sent.</p>
+      <p>Contact our experienced Customer Support team at any time to request self-exclusion. A 7-day cooling-off period is also available.</p>
+
+      <h4>Underage Gambling</h4>
+
+      <p>Players must be of legal gambling age in their jurisdiction (at least 18+) in order to play at girobet.com. It is their responsibility to be aware of the age restriction where they reside and play, and to confirm their legitimacy when creating an account at girobet.com.</p>
+      <p>Parents are advised to:</p>
+      <ul>
+        <li>Password protect computer, mobile, and/or tablet.</li>
+        <li>Do not leave the device unattended when logged into your account.</li>
+        <li>Make sure all account details and credit cards are inaccessible to children.</li>
+        <li>Do not save passwords on your computer; write them down and keep them out of reach.</li>
+        <li>Download filtering software (e.g., Net Nanny) to prevent minors from accessing inappropriate sites.</li>
+      </ul>
+    </WrapperPage>
+  </div>
 </template>
