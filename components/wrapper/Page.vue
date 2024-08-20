4975c51720c68c8e1d8b8f06ecf79de8ca0eb66b
Travis <travis@marketcake.com>
2024-08-20 15:22:16 -0700

    feat: page layout + aml policy

---
+++ components/wrapper/Page.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const { t } = useI18n();
+
+defineProps<{
+  title: string;
+  version?: string;
+  date?: string;
+}>();
+</script>
+
+<template>
+  <div class="text-base md:text-lg px-4 mt-8 pb-14 max-w-[850px] mx-auto">
+    <h1 class="text-3xl mb-6 font-semibold text-white">{{ title }}</h1>
+
+    <div
+      v-if="version || date"
+      class="mb-8 text-lg space-y-1"
+    >
+      <div v-if="version"><span class="text-emphasis font-medium">{{ t('metadata.version') }}:</span> {{ version }}</div>
+      <div v-if="date"><span class="text-emphasis font-medium">{{ t('metadata.updated') }}:</span> {{ date }}</div>
+    </div>
+
+    <div class="text-subtle leading-6 md:leading-7">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+:deep(p) {
+  @apply my-6;
+}
+:deep(strong) {
+  @apply font-semibold;
+}
+:deep(h4) {
+  @apply my-6 text-2xl font-semibold text-emphasis;
+}
+</style>
--- pages/aml-policy.vue
+++ pages/aml-policy.vue
@@ -3,5 +3,175 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <h1>{{ t("page.aml_policy") }}</h1>
+  <WrapperPage
+    :title="t('page.aml_policy')"
+    version="1.0"
+    date="August 20, 2024"
+  >
+    <h4>Objective of the Policy</h4>
+
+    <p>girobet.com is fully committed to being constantly vigilant to prevent money laundering and combat the financing of terrorism in order to minimize and manage risks such as reputational risk, legal risk, and regulatory risk. It is also committed to its social duty to prevent serious crime and not to allow its systems to be abused in furtherance of these crimes. The company will endeavor to keep itself updated with developments both at national and international levels on any initiatives to prevent money laundering and the financing of terrorism. It commits itself to protect, at all times, the organization, its operations, and its reputation from the threat of money laundering, the funding of terrorism, and other criminal activities.</p>
+
+    <h4>Obligations</h4>
+    <p><strong>Appoint a Money Laundering Reporting Officer (MLRO):</strong></p>
+    <p>One of the senior officers will be appointed as the designated MLRO, whose responsibilities include the duties required by the laws, regulations, and guidance notes.</p>
+
+    <p><strong>Reporting Suspected Money Laundering:</strong></p>
+    <p>All instances of suspected Money Laundering attempts must be reported to the MLRO without conveying suspicion to the customer. Further actions must await consent from the relevant authority.</p>
+
+    <p><strong>Establishing Customer Identity:</strong></p>
+    <p>Take reasonable steps to establish the identity of any person for whom it is proposed to provide service. Implement a FULL ‘Know Your Customer’ policy to assist in tackling not just Money Laundering threats but other fraud threats generally.</p>
+
+    <p><strong>Maintain Secure Player List:</strong></p>
+    <p>Keep a secure online list of all registered players at all times.</p>
+
+    <p><strong>Retain Documentation:</strong></p>
+    <p>Retain identification and transactional documentation as defined by the laws, regulations, and guidance notes.</p>
+
+    <p><strong>No Cash Elements:</strong></p>
+    <p>The business will not have any cash elements, mitigating risks associated with counterfeit monies.</p>
+
+    <p><strong>Account Transaction History Monitoring:</strong></p>
+    <p>Systems will be in place to record all transactions for an account, which will be subject to regular and continual checks against fraud and social responsibility indicators.</p>
+
+    <p><strong>High-Risk Customer Monitoring:</strong></p>
+    <p>girobet.com will maintain a register of customers identified as high risk or politically exposed. These accounts will be subject to greater scrutiny and frequent checks.</p>
+
+    <p><strong>Staff Investigations:</strong></p>
+    <p>In cases of illegal or inappropriate behavior by staff, investigatory and disciplinary procedures will follow those outlined in staff contracts, with potential for suspension in cases such as transactional fraud.</p>
+
+    <p><strong>Suspicious Transactions:</strong></p>
+    <p>All transactions carried out on our site may be controlled to avoid money laundering or terrorist financing activities. Suspicious transactions will be reported to the relevant authority.</p>
+
+    <p><strong>Fraud Suspicion:</strong></p>
+    <p>If the risk and fraud department suspects fraud, it may require the player to provide appropriate documentation. The account will be suspended until the situation is cleared, with communication to the relevant authority.</p>
+
+    <p><strong>Staff Training:</strong></p>
+    <p>Provide initial and ongoing training to all relevant staff, including but not limited to:</p>
+    <ul>
+      <li>Staff personal responsibilities</li>
+      <li>Procedures for identifying Players</li>
+      <li>Monitoring Player activity</li>
+      <li>Record-keeping</li>
+      <li>Reporting any unusual/suspicious transactions</li>
+      <li>Monitoring unusual Betting Patterns, suspicious deposit and withdrawal patterns, and reliability of Card Data</li>
+      <li>Addressing customer verification issues and Identity Theft</li>
+      <li>Handling Account linkage/multiple accounting</li>
+      <li>Managing accounts from High-Risk Jurisdictions</li>
+      <li>Conducting Client due diligence (CDD), including enhanced requirements for high-risk clients, such as PEPs</li>
+    </ul>
+
+    <p><strong>Policy Development:</strong></p>
+    <p>Ensure that this policy is developed and maintained in line with evolving statutory and regulatory obligations and advice from the relevant authorities.</p>
+
+    <p><strong>Complex Transaction Examination:</strong></p>
+    <p>Examine with special attention, and to the extent possible, the background and purpose of any complex or large transactions, especially those likely to be related to money laundering or the funding of terrorism.</p>
+
+    <p><strong>Account Termination:</strong></p>
+    <p>girobet.com may terminate Accounts with immediate effect and retain all account funds in the event of credible suspicion of fraudulent activity.</p>
+
+    <p><strong>Suspicious Activity Reporting:</strong></p>
+    <p>Report any suspicion or knowledge of money laundering or terrorist financing to the Financial Intelligence Analysis Unit (FIAU).</p>
+
+    <p><strong>Enhanced Due Diligence Checks (EDD):</strong></p>
+    <p>These checks are subject to players’ profiles and the amount of risk they pose to the company. Enhanced due diligence may include checks on where the customer works, the value of their house, and other relevant factors.</p>
+
+    <p><strong>Confidential Reporting:</strong></p>
+    <p>Suspicious Activity Reports (SARs) should be done confidentially, discreetly, in a handwritten form, and not via email.</p>
+
+    <p><strong>No Disclosure:</strong></p>
+    <p>Employees must not, under any circumstances, disclose or discuss any AML concern with the person(s) subject to an investigation.</p>
+
+    <p><strong>Account Anonymity:</strong></p>
+    <p>No remark should ever be left on an account that would indicate ML is suspected.</p>
+
+    <p><strong>Cooperate with Authorities:</strong></p>
+    <p>Cooperate with all relevant administrative, enforcement, and judicial authorities to prevent and detect criminal activity.</p>
+
+    <h4>Company Guidelines</h4>
+
+    <p><strong>Account Registration:</strong></p>
+    <p>The company shall not accept to open anonymous Accounts or Accounts in fictitious names such that the true beneficial owner is not known.</p>
+
+    <p><strong>No Cash Acceptance:</strong></p>
+    <p>Cash from Players will not be accepted.</p>
+
+    <p><strong>Acceptable Funds:</strong></p>
+    <p>Funds may be received from Players only by approved methods such as credit cards, debit cards, electronic transfer, etc.</p>
+
+    <p><strong>Age Restriction:</strong></p>
+    <p>Not register a Player under eighteen (18) years of age.</p>
+
+    <p><strong>Single Account Policy:</strong></p>
+    <p>Only one account can be registered in the name of a particular person.</p>
+
+    <p><strong>Payment of Winnings:</strong></p>
+    <p>Transfer payments of winnings or refunds back to the same route from where the funds originated, where possible.</p>
+
+    <p><strong>No Wager Acceptance:</strong></p>
+    <p>Not accept a wager unless a User Account has been established in the name of the Player and adequate funds are in the Account.</p>
+
+    <p><strong>Geographical Restriction:</strong></p>
+    <p>Not accept Players residing or playing from non-reputable jurisdictions.</p>
+
+    <p><strong>Verify Identity:</strong></p>
+    <p>Not make payments in excess of EUR 300 until the Player’s identity, age, and place of residence have been verified.</p>
+
+    <p><strong>Close Dormant Accounts:</strong></p>
+    <p>If no transaction is recorded on a User Account for thirty months, close the User Account and remit the balance to the Player or to the regulator if the Player cannot be located.</p>
+
+    <p><strong>Verify Creditworthiness:</strong></p>
+    <p>Where necessary, verify the creditworthiness of the Player with third parties.</p>
+
+    <p><strong>False Information:</strong></p>
+    <p>If a person has provided false information, the company shall immediately cancel that person’s registration as a Player.</p>
+
+    <h4>Customer Declaration</h4>
+
+    <p>The customer declares and expressly guarantees that all the amounts deposited and used to participate in girobet.com remote online games come from legitimate sources and are not connected with any fraudulent activity, money laundering, or illicit activities. girobet.com reserves the right to take any steps it considers necessary to comply with these requirements, including supervising any transfer of funds and retaining all a customer’s funds if they derive from or are related to illicit or fraudulent activities.</p>
+
+    <h4>Deposit and Withdrawal Procedures</h4>
+
+    <p><strong>Currency for Transactions:</strong></p>
+    <p>Deposits and withdrawals are only made in Brazilian Reais (R$)</p>
+
+    <p><strong>Minimum Deposit Amount:</strong></p>
+    <p>The minimum deposit amount is BRL 2</p>
+
+    <p><strong>Responsibility for Bank Fees:</strong></p>
+    <p>It is the customer’s responsibility to check if their bank imposes any fees on transfers.</p>
+
+    <p><strong>Verification of Payment Account:</strong></p>
+    <p>The client must be the holder of the payment account provided to girobet.com.</p>
+
+    <p><strong>Account Balance Management:</strong></p>
+    <p>The total balance of the player account includes Casino Balance, Real Sportsbook Balance, Casino Bonus Balance, and Sportsbook Bonus Balance. The existence of two wallets does not change the behavior of the account during withdrawal requests.</p>
+
+    <p><strong>Withdrawal Methods:</strong></p>
+    <p>Withdrawals must be made by the same method as the deposit. If the deposit method does not allow withdrawals, these must be made by bank transfer.</p>
+
+    <p><strong>Verification of Identity:</strong></p>
+    <p>For the first withdrawal request, players should send an email to support@girobet.com with:</p>
+    <ul>
+      <li>Document of identity with photo and date of birth</li>
+      <li>Proof of address</li>
+      <li>Proof of ownership of the account</li>
+    </ul>
+
+    <p><strong>Account Usage:</strong></p>
+    <p>The Player Account shall not be used as a bank account. In cases of deposits or withdrawals without gaming activity, girobet.com reserves the right to request explanations before transferring the withdrawal request.</p>
+
+    <h4>Politically Exposed Persons (PEP)</h4>
+
+    <p>European guidelines regarding “The Prevention of Laundering the Proceeds from Criminal Activity (Money Laundering) and Terrorism Financing” entered into effect, changing the definition of a politically exposed person. girobet.com is obliged by law to identify which clients/beneficiaries should be considered politically exposed persons, their family members, or close associates.</p>
+    <p>A PEP is defined as a person who holds or has held a prominent public position in a country of the European Union or European Economic Area or a third country.</p>
+    <p>Family members of a PEP include:</p>
+    <ul>
+      <li>A spouse or equivalent under the respective country's laws</li>
+      <li>A child or their spouse</li>
+      <li>A parent, grandparent, or grandchild</li>
+      <li>A brother or sister</li>
+    </ul>
+    <p>A close associate of a PEP is an individual publicly known to maintain a business or close relationship with a PEP or to be a shareholder in the same commercial company.</p>
+  </WrapperPage>
 </template>
