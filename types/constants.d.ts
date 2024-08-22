aab15787da1294e94c21dbaa14eb6c3f8d9cdf95
Travis <travis@marketcake.com>
2024-08-22 05:48:29 -0700

    comment

--- types/constants.d.ts
+++ types/constants.d.ts
@@ -2,6 +2,8 @@
 // - "Jurisdiction" is a backend concept which really bundles multiple things together
 // - Telephone prefix dropdown does not necessarily correlate with jurisdictions OR locales, however it DOES involve masking which likely would need to be on "Country"
 
+// e8c5df6e-64ba-4597-945a-ceb766797805
+
 export type SupportedCurrencyCode =
   | "BRL";
 
