dde38597ce8d1ac456b93483b4c6ad0660820cb7
Travis <travis@marketcake.com>
2024-08-23 07:49:49 -0700

    winning game styling

--- components/WinningNowGame.vue
+++ components/WinningNowGame.vue
@@ -9,20 +9,21 @@
 </script>
 
 <template>
-  <div
-    class="flex flex-row items-center space-x-2 bg-subtle p-2 rounded-[0.7rem] cursor-pointer outline-none"
+  <NuxtLink
+    to="/game/123"
+    class="flex flex-row items-center space-x-3 bg-subtle p-2 rounded-lg cursor-pointer outline-none"
   >
-    <div class="rounded-[0.7rem]">
+    <div class="flex-shrink-0 rounded-[0.7rem]">
       <NuxtImg
         src="/assets/images/games/2.png"
         alt=""
-        class="aspect-[300/350] w-full h-auto object-cover object-center rounded-[0.393rem]"
+        class="aspect-[300/350] w-full h-auto object-cover object-center rounded-default"
       />
     </div>
-    <div>
-      <p class="text-subtle truncate">Golden rush inferno</p>
-      <p>Rui***</p>
-      <p class="text-lg font-medium bg-button-primary text-transparent bg-clip-text">$32.14</p>
+    <div class="font-medium leading-tight space-y-1">
+      <div class="font-semibold">Rui***</div>
+      <div class="text-subtle text-sm">Golden rush inferno</div>
+      <div class="text-lg bg-button-primary text-transparent bg-clip-text">R$ 32.14</div>
     </div>
-  </div>
+  </NuxtLink>
 </template>
