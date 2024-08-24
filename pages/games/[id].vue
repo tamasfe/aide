8d42695ac6f9d66f3cb63aabc0f010111d7264ee
Travis <travis@marketcake.com>
2024-08-23 17:49:21 -0700

    comments

--- pages/games/[id].vue
+++ pages/games/[id].vue
@@ -3,15 +3,17 @@ const { isMobile } = useDevice();
 </script>
 
 <template>
-  <div
-    class="max-w-full sm:px-4 xl:py-8 xl:max-w-[1240px] mx-auto flex flex-col space-y-4 md:space-y-8"
-  >
-    <GameFrameMobile v-if="isMobile" />
-    <GameFrame v-else />
+  <div class="giro__container">
+    <div class="giro__section">
+      <GameFrameMobile v-if="isMobile" />
+      <GameFrame v-else />
+    </div>
 
-    <GameDescriptionCard class="bg-subtle" />
+    <div class="giro__section">
+      <GameDescriptionCard class="bg-subtle" />
+    </div>
 
-    <div class="giro__container w-full flex flex-col space-y-4 md:space-y-8">
+    <div class=".giro__section">
       <WrapperScrollerGame />
     </div>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,8 @@ const search = ref("");
     <CarouselHero />
 
     <div class="giro__container giro__section">
+      <!-- TODO this gap-8 should potentially be giro_section but multiple times -->
+      <!-- find better way to do common containers/common space-y-X -->
       <div class="flex flex-col gap-8">
         <Search
           v-model="search"
