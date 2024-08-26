0c4488a523c69b4440d64e69ffe4e1fbf7eb57e9
Bernardo António Borda d'Água <berna.agua@protonmail.com>
2024-08-26 13:29:42 +0100

    version bumps

--- crates/aide-axum-sqlx-tx/src/lib.rs
+++ crates/aide-axum-sqlx-tx/src/lib.rs
@@ -1,23 +1,27 @@
 #![doc = include_str!("../README.md")]
 
-#[cfg(all(feature = "sqlx-07", feature = "sqlx-06"))]
-compile_error!(
-    "aide-axum-sqlx-tx features \"sqlx-07\" and \"sqlx-06\" cannot be enabled at the same time"
-);
 
-#[cfg(all(not(feature = "sqlx-07"), not(feature = "sqlx-06")))]
-compile_error!("aide-axum-sqlx-tx feature \"sqlx-07\" or \"sqlx-06\" must be enabled");
+#[cfg(all(not(feature = "axum-sqlx-tx-09"), not(feature = "axum-sqlx-tx-08"), not(feature = "axum-sqlx-tx-07"), not(feature = "axum-sqlx-tx-06")))]
+compile_error!("aide-axum-sqlx-tx feature \"axum-sqlx-tx-09\", \"axum-sqlx-tx-08\", \"axum-sqlx-tx-07\", \"axum-sqlx-tx-06\" must be enabled");
 
 use aide::NoApi;
 
-#[cfg(feature = "sqlx-06")]
-use sqlx_06 as tx;
-#[cfg(all(feature = "sqlx-07", not(feature = "sqlx-06")))]
-use sqlx_07 as tx;
+#[cfg(all(feature = "axum-sqlx-tx-06", not(all(feature = "axum-sqlx-tx-09", feature = "axum-sqlx-tx-08", feature = "axum-sqlx-tx-07", feature = "axum-sqlx-tx-06"))))]
+use axum_sqlx_tx_06 as tx;
 
-#[cfg(any(feature = "sqlx-07", feature = "sqlx-06"))]
+#[cfg(all(feature = "axum-sqlx-tx-07", not(all(feature = "axum-sqlx-tx-09", feature = "axum-sqlx-tx-08", feature = "axum-sqlx-tx-07", feature = "axum-sqlx-tx-06"))))]
+use axum_sqlx_tx_07 as tx;
+
+#[cfg(all(feature = "axum-sqlx-tx-08", not(all(feature = "axum-sqlx-tx-09", feature = "axum-sqlx-tx-08", feature = "axum-sqlx-tx-07", feature = "axum-sqlx-tx-06"))))]
+use axum_sqlx_tx_08 as tx;
+
+#[cfg(all(feature = "axum-sqlx-tx-09", not(all(feature = "axum-sqlx-tx-09", feature = "axum-sqlx-tx-08", feature = "axum-sqlx-tx-07", feature = "axum-sqlx-tx-06"))))]
+use axum_sqlx_tx_09 as tx;
+
+// compile with `--all--features`
+#[cfg(all(feature = "axum-sqlx-tx-09", feature = "axum-sqlx-tx-08", feature = "axum-sqlx-tx-07", feature = "axum-sqlx-tx-06"))]
+use axum_sqlx_tx_09 as tx;
+
 #[doc(inline)]
 pub use tx::{Error, Layer, Service};
-
-#[cfg(any(feature = "sqlx-07", feature = "sqlx-06"))]
 pub type Tx<T> = NoApi<tx::Tx<T>>;
