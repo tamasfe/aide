e3273777c8e6c65800b299a848c42e08c9513e0b
Travis <travis@marketcake.com>
2024-08-25 18:32:59 -0700

    prepare for register

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // const modal = ref();
-const modal = ref("login");
-// const modal = ref("register");
+// const modal = ref("login");
+const modal = ref("register");
 // const modal = ref("cancel_reg");
 // const modal = ref("forgot_password");
 // const modal = ref("invite");
@@ -16,24 +16,24 @@ const modal = ref("login");
     <ModalLogin
       v-if="modal === 'login'"
     />
-    <!-- <ModalRegister -->
-    <!--   v-if="modal === 'register'" -->
-    <!-- /> -->
-    <!-- <ModalCancelRegistration -->
-    <!--   v-if="modal === 'cancel_reg'" -->
-    <!-- /> -->
-    <!-- <ModalForgotPassword -->
-    <!--   v-if="modal === 'forgot_password'" -->
-    <!-- /> -->
-    <!-- <ModalInvite -->
-    <!--   v-if="modal === 'invite'" -->
-    <!-- /> -->
-    <!-- <ModalDeposit -->
-    <!--   v-if="modal === 'deposit'" -->
-    <!-- /> -->
-    <!-- <ModalWithdrawal -->
-    <!--   v-if="modal === 'withdrawal'" -->
-    <!-- /> -->
+    <ModalRegister
+      v-if="modal === 'register'"
+    />
+    <ModalCancelRegistration
+      v-if="modal === 'cancel_reg'"
+    />
+    <ModalForgotPassword
+      v-if="modal === 'forgot_password'"
+    />
+    <ModalInvite
+      v-if="modal === 'invite'"
+    />
+    <ModalDeposit
+      v-if="modal === 'deposit'"
+    />
+    <ModalWithdrawal
+      v-if="modal === 'withdrawal'"
+    />
 
     <NuxtLayout>
       <NuxtPage />
