d3ddf438139de27251b85b74002b711105b9844e
Travis <travis@marketcake.com>
2024-08-26 16:34:18 -0700

    finish limits

--- components/form/Withdrawal.vue
+++ components/form/Withdrawal.vue
@@ -37,7 +37,7 @@ const onToggleLimits = () => {
           name="lucide:info"
           size="18"
         />
-        <div>limits</div>
+        <div>{{ t('modal_payments.limits') }}</div>
       </div>
     </div>
 
@@ -75,105 +75,15 @@ const onToggleLimits = () => {
       </BaseButtonNew>
     </template>
     <template v-else>
-      hi
+      <InfoWithdrawalLimits />
+
+      <BaseButtonNew
+        variant="subtle"
+        class="mt-2"
+        @click="onToggleLimits"
+      >
+        {{ t("button.go_back") }}
+      </BaseButtonNew>
     </template>
   </BaseForm>
 </template>
-
-<!-- <script setup lang="ts"> -->
-<!-- import { PhQuestion } from "@phosphor-icons/vue"; -->
-<!-- import * as zod from "zod"; -->
-<!---->
-<!-- const { t } = useI18n(); -->
-<!---->
-<!-- const emit = defineEmits(["click:info", "click:cancel"]); -->
-<!---->
-<!-- const validationSchema = toTypedSchema( -->
-<!--   zod.object({ -->
-<!--     value: zod.string().min(1, { message: t("validation.field_required") }), -->
-<!--   }), -->
-<!-- ); -->
-<!-- const { handleSubmit, errors } = useForm({ -->
-<!--   validationSchema, -->
-<!-- }); -->
-<!-- const { value } = useField( -->
-<!--   "value", -->
-<!--   {}, -->
-<!--   { -->
-<!--     initialValue: "100.00", -->
-<!--   }, -->
-<!-- ); -->
-<!---->
-<!-- const cancel = () => { -->
-<!--   emit("click:cancel"); -->
-<!-- }; -->
-<!---->
-<!-- const onSubmit = handleSubmit((values) => { -->
-<!--   alert(JSON.stringify(values, null, 2)); -->
-<!-- }); -->
-<!-- </script> -->
-<!---->
-<!-- <template> -->
-<!--   <form -->
-<!--     class="flex flex-col gap-4 w-full" -->
-<!--     @submit="onSubmit" -->
-<!--   > -->
-<!--     <div class="flex items-center justify-between text-subtle"> -->
-<!--       <p>Pix Key: <span class="text-emphasis">078.843.426-85</span></p> -->
-<!--       <NuxtLink -->
-<!--         to="/profile/settings" -->
-<!--         type="button" -->
-<!--         class="outline-none" -->
-<!--       >Edit</NuxtLink> -->
-<!--     </div> -->
-<!--     <p class="text-subtle"> -->
-<!--       My CPF: <span class="text-emphasis">078.843.426-85</span> -->
-<!--     </p> -->
-<!--     <div -->
-<!--       class="p-4 bg-warning flex items-center justify-between rounded-default text-warning" -->
-<!--     > -->
-<!--       <p>Withdrawal Limits</p> -->
-<!--       <button -->
-<!--         type="button" -->
-<!--         class="outline-none" -->
-<!--         title="There are some limits" -->
-<!--         @click="emit('click:info')" -->
-<!--       > -->
-<!--         <PhQuestion :size="24" /> -->
-<!--       </button> -->
-<!--     </div> -->
-<!--     <BaseInputGroup -->
-<!--       v-model="value" -->
-<!--       class="w-full" -->
-<!--       type="number" -->
-<!--       wrapper-class="bg-subtle text-lg" -->
-<!--       input-class="font-semibold" -->
-<!--       :placeholder="t('placeholder.withdrawal_amount')" -->
-<!--       :error="errors.value" -->
-<!--     > -->
-<!--       <template #prefix> -->
-<!--         <IconRSGreen /> -->
-<!--       </template> -->
-<!--     </BaseInputGroup> -->
-<!--     <p class="text-subtle"> -->
-<!--       {{ t('modal_payments.available_withdrawal') }}: -->
-<!--       <span class="text-emphasis">R$ 500,00</span> -->
-<!--     </p> -->
-<!--     <BaseButton -->
-<!--       variant="primary" -->
-<!--       big -->
-<!--       shadow -->
-<!--       class="w-full inline-flex justify-center text-base sm:text-lg" -->
-<!--     > -->
-<!--       {{ t('button.withdraw') }} -->
-<!--     </BaseButton> -->
-<!--     <BaseButton -->
-<!--       big -->
-<!--       shadow -->
-<!--       class="w-full bg-muted text-emphasis inline-flex justify-center text-base sm:text-lg" -->
-<!--       @click="cancel" -->
-<!--     > -->
-<!--       {{ t('button.cancel') }} -->
-<!--     </BaseButton> -->
-<!--   </form> -->
-<!-- </template> -->
---
+++ components/info/WithdrawalLimits.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// DESIGN STATUS:       ✅
+// ARCHITECTURE STATUS: ✅
+// TRANSLATION STATUS:  ✴️
+</script>
+
+<template>
+  <div class="grid grid-cols-[max-content_max-content] auto-cols-min mb-6 text-base text-subtle leading-loose">
+    <div>Minimum Withdrawal</div>
+    <div class="value">R$ 20</div>
+    <div>Maximum Withdrawal</div>
+    <div class="value">R$ 4,999</div>
+    <div>Number of Withdrawals</div>
+    <div class="value">1/day</div>
+    <div>Bets to Enable Withdrawal</div>
+    <div class="value">1.7x</div>
+    <div>Minimum Withdrawal</div>
+    <div class="value">R$ 20</div>
+  </div>
+  <div class="flex flex-col">
+    <div class="leading-relaxed text-[0.78rem] text-subtle-light">When withdrawing funds from your account, please follow these rules to ensure a smooth and secure transaction process. The minimum withdrawal amount is specified on the website and may vary depending on the withdrawal method chosen. Withdrawals are processed only to the payment method originally used for depositing funds, unless otherwise specified or if the original method does not support withdrawals...</div>
+  </div>
+</template>
+
+<style scoped>
+.value {
+  @apply pl-6 text-white font-medium;
+}
+</style>
