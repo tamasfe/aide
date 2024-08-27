59081febd02c115e298fac0f39eeb5fed49526e3
Travis <travis@marketcake.com>
2024-08-27 07:24:42 -0700

    comment

--- components/form/DepositConfirm.vue
+++ components/form/DepositConfirm.vue
@@ -3,6 +3,7 @@
 //   * make every single part of the input/copy icon/button clickable to copy
 //   * the inputgroup should also be set to text-subtle... but i dont like adding tons of "input-class, icon-class blah blah blah"
 //   * after you style the color, the link should also be text-sm
+//   * after that... the input + copy icon ALSO needs cursor-pointer
 // ARCHITECTURE STATUS: ✴️
 //   * implement real countdown (see on bet7k what happens when it runs out)
 //   * G branded QR is VERY FUCKING COOL and adds a serious bit of chrome
@@ -39,23 +40,20 @@
 
     <div class="text-4xl font-bold bg-button-emphasis text-transparent bg-clip-text">R$ 100,00</div>
 
-    <div class="cursor-pointer w-full">
-      <BaseInputGroup
-        autocomplete="off"
-        placeholder-placement="default"
-        class="cursor-pointer"
-        :disabled="true"
-      >
-        <template #suffix>
-          <div class="ml-5 flex flex-row justify-center items-center">
-            <Icon
-              name="lucide:copy"
-              size="24"
-            />
-          </div>
-        </template>
-      </BaseInputGroup>
-    </div>
+    <BaseInputGroup
+      autocomplete="off"
+      placeholder-placement="default"
+      :disabled="true"
+    >
+      <template #suffix>
+        <div class="ml-5 flex flex-row justify-center items-center">
+          <Icon
+            name="lucide:copy"
+            size="24"
+          />
+        </div>
+      </template>
+    </BaseInputGroup>
 
     <BaseButton
       size="xl"
