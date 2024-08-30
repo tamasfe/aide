21a7f55f66c1c6d998d722242da2f01141bb9e34
Travis <travis@marketcake.com>
2024-08-30 15:17:24 -0700

    comment

--- app/components/SearchResults.vue
+++ app/components/SearchResults.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 // DESIGN STATUS: ✴️
-//   * use skeleton loaders and loading spinner inside search bar
+//   * use skeleton loaders and loading spinner inside search bar IF it looks good. loading spinner in search input might give impression that its "slow" (bitstarz doesnt do this and it feels faster psychologically.... will just have to see it and decide)
 
 const data = ref(
   new Array(30).fill(0).map((d, i) => ({
