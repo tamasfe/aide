98874d26e3ae823def2b4f29aa1d9251557d7bb9
Travis <travis@marketcake.com>
2024-08-31 08:20:39 -0700

    refactor dashboard section/section item

--- app/components/DashboardSection.vue
+++ app/components/DashboardSection.vue
@@ -5,11 +5,11 @@ defineProps<{
 </script>
 
 <template>
-  <div class="px-4 sm:px-8 py-4 space-y-8 bg-subtle rounded-2xl">
-    <div class="text-[1.137rem] font-bold">
+  <div class="flex flex-col gap-8 px-4 sm:px-8 py-4 bg-subtle rounded-lg">
+    <div class="text-xl text-white font-semibold">
       <h2>{{ title }}</h2>
     </div>
-    <div class="space-y-8 sm:space-y-4">
+    <div class="flex flex-col gap-8 sm:gap-4">
       <slot />
     </div>
   </div>
--- app/components/DashboardSectionItem.vue
+++ app/components/DashboardSectionItem.vue
@@ -5,15 +5,18 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex flex-col sm:flex-row justify-between gap-x-28 gap-y-3">
-    <div class="sm:w-[15%] text-subtle font-medium">
-      <h4>{{ name }}</h4>
+  <div class="flex flex-col sm:flex-row justify-between gap-x-28 gap-y-1">
+    <div class="sm:w-[4rem] text-subtle font-medium whitespace-nowrap">
+      <h4 class="font-semibold text-[0.80rem]">{{ name }}</h4>
     </div>
     <div class="sm:flex-1 space-y-1">
-      <div class="text-emphasis font-medium text-lg">
+      <div class="text-white font-medium">
         <slot />
       </div>
-      <div class="text-subtle">
+      <div
+        v-if="$slots.description"
+        class="text-subtle"
+      >
         <slot name="description" />
       </div>
     </div>
