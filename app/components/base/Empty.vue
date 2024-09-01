ed109913ff7f60b18cb30c095e1055903ffdd72a
Travis <travis@marketcake.com>
2024-09-01 07:27:07 -0700

    add Empty component

---
+++ app/components/base/Empty.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    title?: string;
+    icon?: string;
+  }>(), {
+    title: "No Results",
+    icon: "lucide:box-select",
+  },
+);
+</script>
+
+<template>
+  <div class="'min-h-[200px] flex flex-row justify-center items-center'">
+    <Icon
+      :name="icon"
+      size="30"
+    />
+    <div class="font-semibold">{{ title }}</div>
+  </div>
+</template>
