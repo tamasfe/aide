3cceb3fc6b617d48179427c1bc7acfdcb5ecf1cb
Travis <travis@marketcake.com>
2024-09-02 15:01:30 -0700

    dash nav scrolls off screen

--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -7,11 +7,17 @@ defineProps<{
 </script>
 
 <template>
-  <div class="py-8 giro__container giro__sections">
-    <NavDashboard
-      :section="section"
-    />
+  <div class="w-full py-8">
+    <!-- so right side of menu goes off screen -->
+    <div class="mb-[var(--giro-section-gap)] pl-[var(--giro-container-padding)]">
+      <NavDashboard
+        :section="section"
+        class="pr-[var(--giro-container-padding)]"
+      />
+    </div>
 
-    <slot />
+    <div class="w-full giro__container giro__sections">
+      <slot />
+    </div>
   </div>
 </template>
