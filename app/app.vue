67b0b27e24c77f22adb71e380706be853e0eace3
Ivan <ivanprats@hey.com>
2024-09-07 17:51:21 +0200

    chore: add prettier as default code formatter
    In a future call we should run the `pnpm format` command to format all
    of the codebase following the configs in the `.prettierrc.json` file.
    
    Then we can tweak the configs to match our coding style and leave it at
    that.

--- app/app.vue
+++ app/app.vue
@@ -24,30 +24,14 @@ $dependencies.asyncMessagePublisher.subscribe(
   <div>
     <NavSidebar />
 
-    <ModalLogin
-      v-if="modal === 'login'"
-    />
-    <ModalRegister
-      v-if="modal === 'register'"
-    />
-    <ModalForgotPassword
-      v-if="modal === 'forgot'"
-    />
-    <ModalCancelRegistration
-      v-if="modal === 'cancel_reg'"
-    />
-    <ModalDeposit
-      v-if="modal === 'deposit'"
-    />
-    <ModalDepositConfirm
-      v-if="modal === 'deposit_confirm'"
-    />
-    <ModalWithdrawal
-      v-if="modal === 'withdrawal'"
-    />
-    <ModalRestrictExpanding
-      v-if="modal === 'restrict_expanding'"
-    />
+    <ModalLogin v-if="modal === 'login'" />
+    <ModalRegister v-if="modal === 'register'" />
+    <ModalForgotPassword v-if="modal === 'forgot'" />
+    <ModalCancelRegistration v-if="modal === 'cancel_reg'" />
+    <ModalDeposit v-if="modal === 'deposit'" />
+    <ModalDepositConfirm v-if="modal === 'deposit_confirm'" />
+    <ModalWithdrawal v-if="modal === 'withdrawal'" />
+    <ModalRestrictExpanding v-if="modal === 'restrict_expanding'" />
     <ModalRestrictLicenseAlternative
       v-if="modal === 'restrict_license_alternative'"
     />
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,8 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 
 // see settings in nuxt.config.js
 
-export default withNuxt()
-  .override("nuxt/vue/rules", {
-    rules: {
-      "vue/singleline-html-element-content-newline": "off",
-    },
-  });
+export default withNuxt().override("nuxt/vue/rules", {
+  rules: {
+    "vue/singleline-html-element-content-newline": "off",
+  },
+});
