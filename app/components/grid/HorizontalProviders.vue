8bf10496c55cb93a29ee7ed41f11e7c4b0d0b82d
Ivan <ivanprats@hey.com>
2024-11-05 11:50:10 +0100

    fix: missing attrs in HorizontalProviders

--- app/components/grid/HorizontalProviders.vue
+++ app/components/grid/HorizontalProviders.vue
@@ -2,34 +2,35 @@
 const { $dependencies } = useNuxtApp();
 const { t } = useI18n();
 
-const loading = ref(false);
+const loading = ref(true);
 const nextProvidersPageToSearch = useState(`grid-horizontal-providers-next-page`, () => 0);
 const providerIds = useState<number[]>(`grid-horizontal-providers-ids`, () => []);
 const canLoadMore = useState(`grid-horizontal-providers-can-load-more`, () => true);
 
-const BLOCK_SERVER_SIDE_REQUEST = false;
-const BLOCK_CLIENT_SIDE_REQUEST = false;
+const ENABLE_SERVER_SIDE_RENDERING = false;
+const DEFER_CLIENT_SIDE_LOADING = true;
 
 const onLoadData = async () => {
   if (!canLoadMore.value) return;
-  if (loading.value) return;
   loading.value = true;
 
   const { providers: foundProviders, canLoadMore: updatedCanLoadMore } = await $dependencies.providers.ui.searchAllProvidersOnHorizontalGrid.handle(nextProvidersPageToSearch.value);
-  providerIds.value.push(...foundProviders.map(game => game.id));
+  providerIds.value.push(...foundProviders.map(provider => provider.id));
   canLoadMore.value = updatedCanLoadMore;
   nextProvidersPageToSearch.value += 1;
 
   loading.value = false;
 };
 
-await useAsyncData(`load-providers`, () => onLoadData().then(() => true), { lazy: !BLOCK_CLIENT_SIDE_REQUEST, server: BLOCK_SERVER_SIDE_REQUEST });
+await useAsyncData(`load-providers`, () => onLoadData().then(() => true), { lazy: DEFER_CLIENT_SIDE_LOADING, server: ENABLE_SERVER_SIDE_RENDERING });
 </script>
 
 <template>
   <GridHeaderHorizontal
-    v-show="providerIds.length > 0"
+    v-show="loading || providerIds.length > 0"
+    :can-load-more="canLoadMore"
     :data="providerIds"
+    :slides-before-load="10"
     :columns="{
       sm: 2.7,
       md: 2.7,
@@ -45,6 +46,7 @@ await useAsyncData(`load-providers`, () => onLoadData().then(() => true), { lazy
     :loading="loading"
     :gap="2"
     aspect-ratio="16/9"
+    @trigger:load="onLoadData"
   >
     <template #title>
       <GridHeaderTitle :title="t('grid.providers')" />
