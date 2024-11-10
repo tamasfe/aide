ac787517df904ef06ce73aa8790dab8be80020f5
Travis <travis@marketcake.com>
2024-11-10 10:14:22 -0800

    style: setting height

--- app/components/DashboardSectionItem.vue
+++ app/components/DashboardSectionItem.vue
@@ -7,7 +7,7 @@ defineProps<{
 <template>
   <div class="flex flex-col sm:flex-row justify-between gap-x-28 gap-y-1">
     <div class="sm:w-[4rem] text-subtle font-medium whitespace-nowrap">
-      <h4 class="font-semibold text-[0.80rem]">{{ name }}</h4>
+      <h4 class="font-semibold text-[0.80rem] sm:min-h-[32px]">{{ name }}</h4>
     </div>
     <div class="sm:flex-1 space-y-1">
       <div class="text-white font-medium">
