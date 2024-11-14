ee49ed82267fbcc5843c53b096dc13b0780a1c11
Ivan <ivanprats@hey.com>
2024-11-14 15:27:03 +0100

    chore: not send jurisdiction error to sentry

--- app/packages/logger/LoggerMiddlewareSentryErrorCapturer.ts
+++ app/packages/logger/LoggerMiddlewareSentryErrorCapturer.ts
@@ -8,6 +8,9 @@ export class LoggerMiddlewareSentryErrorCapturer implements LoggerMiddleware {
   public callback(_level: LogLevel, _message: string, _error: CustomError | undefined, _data: Record<string, unknown>): ((level: LogLevel, message: string, error: CustomError | undefined, data: Record<string, unknown>) => void) {
     return (level: LogLevel, message: string, error: CustomError | undefined, data: Record<string, unknown>) => {
       if (level === "error" && error) {
+        if (error.name === "ErrorJurisdictionIsNotSupported") {
+          return;
+        }
         this.sentryCaptureException(error, (scope) => {
           scope.setContext("Log", { data, message });
           scope.setContext("Error metadata", error.toJSON());
