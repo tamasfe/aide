d3373d98a4fa00338b6564ceb6087176a98065a7
Ivan <ivanprats@hey.com>
2024-12-03 20:30:23 +0100

    improv: notify to sentry InfrastructureError's cause

--- app/packages/logger/LoggerMiddlewareSentryErrorCapturer.ts
+++ app/packages/logger/LoggerMiddlewareSentryErrorCapturer.ts
@@ -10,7 +10,17 @@ export class LoggerMiddlewareSentryErrorCapturer implements LoggerMiddleware {
   public callback(_level: LogLevel, _message: string, _error: CustomError | undefined, _data: Record<string, unknown>): ((level: LogLevel, message: string, error: CustomError | undefined, data: Record<string, unknown>) => void) {
     return (level: LogLevel, message: string, error: CustomError | undefined, data: Record<string, unknown>) => {
       if (level === "error" && error) {
-        if (error instanceof InfrastructureError && error.cause instanceof HttpBackendApiError && error.cause.isJurisdictionNotSupportedError) {
+        if (error instanceof InfrastructureError) {
+          if (error.cause instanceof HttpBackendApiError && error.cause.isJurisdictionNotSupportedError) {
+            return;
+          }
+          // Capture the cause of the InfrastructureError instead of the InfrastructureError itself
+          this.sentryCaptureException(error.cause, (scope) => {
+            scope.setContext("Log", { data, message });
+            scope.setContext("Error metadata", { ...("metadata" in error.cause ? error.cause.metadata : error.cause) });
+            scope.setContext("Parent InfrastructureError metadata", error.toJSON());
+            return scope;
+          });
           return;
         }
         this.sentryCaptureException(error, (scope) => {
