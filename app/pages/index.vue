aa768df4888f16c517034c8d33ad4665a3b93311
Daniel Schneider <daniel.schneider@eramux.com>
2025-01-04 18:40:32 +0100

    feat: ssr games

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const { $dependencies } = useNuxtApp();
 
-const ENABLE_SERVER_SIDE_RENDERING = false;
+const ENABLE_SERVER_SIDE_RENDERING = true;
 const DEFER_CLIENT_SIDE_LOADING = true;
 const { data: categories } = await useAsyncData("home-category-identifiers", async () => {
   return $dependencies.games.ui.searchGameCategoriesByGroup.handle("home", true);
