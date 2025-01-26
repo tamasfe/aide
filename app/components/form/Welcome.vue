b6d61dc8d73665f7a608904ee548a40b146f28b5
Ivan <ivanprats@hey.com>
2025-01-26 16:31:17 +0100

    feat: add register now button to home slider side
    Missing to do: the CSS animation like Roobet's main button

---
+++ app/components/form/Welcome.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { $dependencies } = useNuxtApp();
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-y-3 text-center">
+    <h1 class="text-2xl md:text-3xl font-semibold text-white">{{ $t("welcome_form.title") }}</h1>
+
+    <BaseButton
+      variant="primary"
+      @click="() => $dependencies.users.ui.emitCommandOpenUserActionModal.handle('register')"
+    >
+      {{ $t("button.register_now") }}
+    </BaseButton>
+
+    <p class="text-subtle">{{ $t("welcome_form.description") }}</p>
+  </div>
+</template>
+
+<style scoped>
+/*
+Here implement the animation from the main primary bottom from https://roobet.com/
+*/
+</style>
--- app/layouts/carousel.vue
+++ app/layouts/carousel.vue
@@ -1,7 +1,17 @@
+<script setup lang="ts">
+const userStore = useUserStore();
+
+const showFormWelcome = computed(() => userStore.isAuthenticated !== true);
+</script>
+
 <template>
   <div class="md:py-8">
     <div class="mb-[var(--giro-section-gap)]">
-      <CarouselHero />
+      <div v-if="showFormWelcome" class="w-full space-y-7 md:space-y-0 md:grid md:grid-cols-3">
+        <CarouselHero class="md:col-span-2" />
+        <FormWelcome />
+      </div>
+      <CarouselHero v-else />
     </div>
 
     <slot />
