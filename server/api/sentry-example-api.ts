a60310a0c7b5a77741ec1695634b98f0f412da89
Ivan <ivanprats@hey.com>
2025-03-03 21:18:05 +0100

    Revert "feat: update and adapt sentry tracing"
    This reverts commit dfc6534c5c9f4d59265970cec9929bf920ddad5e.

--- server/api/sentry-example-api.ts
+++
@@ -1,6 +0,0 @@
-// This is just a very simple API route that throws an example error.
-// Feel free to delete this file.
-
-export default defineEventHandler(() => {
-  throw new Error("Sentry Example API Route Error");
-});
