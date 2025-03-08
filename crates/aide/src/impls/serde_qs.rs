5728266f58359a2793edf6d29be781b8ad93ca10
Luka Blašković <lblasc@tvbeat.com>
2025-03-08 09:22:47 +0000

    Bring back serde_qs, bump to v0.14
    - additionally implement OperationInput for OptionalQsQuery

---
+++ crates/aide/src/impls/serde_qs.rs
@@ -0,0 +1,31 @@
+use schemars::JsonSchema;
+
+use crate::{
+    openapi::Operation,
+    operation::{add_parameters, parameters_from_schema, ParamLocation},
+    OperationInput,
+};
+
+#[cfg(feature = "axum")]
+impl<T> OperationInput for serde_qs::axum::QsQuery<T>
+where
+    T: JsonSchema,
+{
+    fn operation_input(ctx: &mut crate::generate::GenContext, operation: &mut Operation) {
+        let schema = ctx.schema.subschema_for::<T>().into_object();
+        let params = parameters_from_schema(ctx, schema, ParamLocation::Query);
+        add_parameters(ctx, operation, params);
+    }
+}
+
+#[cfg(feature = "axum")]
+impl<T> OperationInput for serde_qs::axum::OptionalQsQuery<T>
+where
+    T: JsonSchema,
+{
+    fn operation_input(ctx: &mut crate::generate::GenContext, operation: &mut Operation) {
+        let schema = ctx.schema.subschema_for::<T>().into_object();
+        let params = parameters_from_schema(ctx, schema, ParamLocation::Query);
+        add_parameters(ctx, operation, params);
+    }
+}
