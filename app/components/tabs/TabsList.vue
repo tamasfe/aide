df71d5a28bdfff69852f7bbc50fc5b7c0ce41d55
Ivan <ivanprats@hey.com>
2025-03-20 20:21:11 -0300

    improv: custom CSS for TabsList scrollbar
    So that user can see there is an horizontal scroll, without looking
    too bad or out-of-style with the rest of the app.

--- app/components/tabs/TabsList.vue
+++ app/components/tabs/TabsList.vue
@@ -24,7 +24,7 @@ const delegatedProps = computed(() => {
     <div
       v-bind="delegatedProps"
       :class="cn(
-        'w-full inline-flex items-center justify-between rounded-lg bg-button-secondary px-2 py-1.5 text-muted-foreground space-x-2 overflow-x-auto md:overflow-x-hidden',
+        'w-full inline-flex items-center justify-between bg-button-secondary px-2 py-1.5 text-muted-foreground space-x-2 overflow-x-scroll md:overflow-x-hidden custom-scrollbar rounded-t-lg rounded-b-none md:rounded-lg',
         props.class,
       )"
     >
@@ -33,3 +33,25 @@ const delegatedProps = computed(() => {
     <slot name="suffix" />
   </TabsList>
 </template>
+
+<style scoped>
+  .custom-scrollbar::-webkit-scrollbar {
+    width: 1px;
+    height: 1px;
+  }
+
+  .custom-scrollbar::-webkit-scrollbar-track {
+    border-radius: 0rem;
+    background: var(--giro-button-secondary);
+  }
+
+  .custom-scrollbar::-webkit-scrollbar-thumb {
+    background: var(--giro-button-secondary);
+    border-radius: 0rem;
+    border: 1px solid var(--giro-button-text-secondary);
+  }
+
+  .custom-scrollbar::-webkit-scrollbar-thumb:hover {
+    background: var(--giro-button-secondary);
+  }
+</style>
