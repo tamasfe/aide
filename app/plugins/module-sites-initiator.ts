86a315a9ba47597486b288c39ced747a5de8502a
Ivan <ivanprats@hey.com>
2025-03-29 10:47:14 -0300

    improv: simpler site-specific asset loading

--- app/plugins/module-sites-initiator.ts
+++ app/plugins/module-sites-initiator.ts
@@ -5,31 +5,29 @@ export default defineNuxtPlugin({
   async setup(_nuxtApp) {
     const siteStore = useSiteStore();
 
-    const ENABLE_SERVER_SIDE_RENDERING = true;
-    const DEFER_CLIENT_SIDE_LOADING = false;
     await useAsyncData("site-store-refresh",
       async () => {
         await siteStore.refresh();
-
-        useHead({
-          link: [
-            {
-              rel: "icon",
-              type: "image/png",
-              href: `/assets/${siteStore.site.identifier}/favicon.png`,
-            },
-            {
-              rel: "stylesheet",
-              type: "text/css",
-              href: `/assets/${siteStore.site.identifier}/variables.css`,
-            },
-          ],
-        });
-
         return 1;
       },
-      { lazy: DEFER_CLIENT_SIDE_LOADING, server: ENABLE_SERVER_SIDE_RENDERING },
     );
+
+    if (import.meta.server) {
+      useHead({
+        link: [
+          {
+            rel: "icon",
+            type: "image/png",
+            href: `/assets/${siteStore.site.identifier}/favicon.png`,
+          },
+          {
+            rel: "stylesheet",
+            type: "text/css",
+            href: `/assets/${siteStore.site.identifier}/variables.css`,
+          },
+        ],
+      });
+    }
 
     return {};
   },
