1cb9b1601c7f70be0b269aeaef0a43965d46d744
Ivan <ivanprats@hey.com>
2025-05-28 16:28:18 +0200

    feat: add home hero click cbs

--- app/components/slide/ChooseGiroBet.vue
+++ app/components/slide/ChooseGiroBet.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
 const siteStore = useSiteStore();
+const { $dependencies } = useNuxtApp();
+const userStore = useUserStore();
+
+const onSlideClick = () => {
+  if (userStore.user) {
+    $dependencies.common.asyncMessagePublisher.emit("frontend:commands:modals:open-user-interaction-modal", { modal: "deposit" });
+    return;
+  }
+  $dependencies.common.asyncMessagePublisher.emit("frontend:commands:modals:open-user-interaction-modal", { modal: "login" });
+};
 </script>
 
 <template>
-  <BaseLink
-    :to="{
-      name: 'todo',
-    }"
+  <button
+    type="button"
+    @click="() => onSlideClick()"
   >
     <NuxtImg
       :src="siteStore.getAssetPath('images/slides/choose-us.jpg')"
       alt=""
       class="rounded-sm md:rounded-lg object-cover object-center w-full h-full"
     />
-  </BaseLink>
+  </button>
 </template>
