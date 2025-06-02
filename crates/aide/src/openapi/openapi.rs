375c3c8bbbe4cf81a6e3356e9389d79d8c695fcc
Jonas Platte <jplatte+git@posteo.de>
2025-06-02 23:02:25 +0200

    Improve readability of OpenApi definition

--- crates/aide/src/openapi/openapi.rs
+++ crates/aide/src/openapi/openapi.rs
@@ -9,23 +9,28 @@ pub struct OpenApi {
     #[serde(with = "serde_version")]
     #[schemars(with = "&'static str")]
     pub openapi: Cow<'static, str>,
+
     /// REQUIRED. Provides metadata about the API.
     /// The metadata MAY be used by tooling as required.
     pub info: Info,
+
     /// The default value for the `$schema` keyword within Schema Objects
     /// contained within this OAS document. This MUST be in the form of a URI.
     #[serde(rename = "jsonSchemaDialect")]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub json_schema_dialect: Option<String>,
+
     /// An array of Server Objects, which provide connectivity information to a
     /// target server. If the servers property is not provided, or is an empty
     /// array, the default value would be a Server Object with a url value of /.
     #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
     pub servers: Vec<Server>,
+
     /// The available paths and operations for the API.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub paths: Option<Paths>,
+
     /// The incoming webhooks that MAY be received as part of this API and that
     /// the API consumer MAY choose to implement. Closely related to the
     /// `callbacks` feature, this section describes requests initiated other
@@ -36,19 +41,23 @@ pub struct OpenApi {
     #[serde(default)]
     #[serde(skip_serializing_if = "IndexMap::is_empty")]
     pub webhooks: IndexMap<String, ReferenceOr<PathItem>>,
+
     /// An element to hold various schemas for the document.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub components: Option<Components>,
+
     /// A declaration of which security mechanisms can be used across the API.
+    ///
     /// The list of values includes alternative security requirement objects
     /// that can be used. Only one of the security requirement objects need to
     /// be satisfied to authorize a request. Individual operations can override
     /// this definition. Global security settings may be overridden on a
     /// per-path basis.
-    #[serde(default)]
-    #[serde(skip_serializing_if = "Vec::is_empty")]
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub security: Vec<SecurityRequirement>,
+
     /// A list of tags used by the document with additional metadata.
+    ///
     /// The order of the tags can be used to reflect on their order by the
     /// parsing tools. Not all tags that are used by the Operation Object
     /// must be declared. The tags that are not declared MAY be organized
@@ -57,10 +66,12 @@ pub struct OpenApi {
     #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
     pub tags: Vec<Tag>,
+
     /// Additional external documentation.
     #[serde(rename = "externalDocs")]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub external_docs: Option<ExternalDocumentation>,
+
     /// Inline extensions to this object.
     #[serde(flatten, deserialize_with = "crate::util::deserialize_extensions")]
     pub extensions: IndexMap<String, serde_json::Value>,
