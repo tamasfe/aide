f9116ba8b2d6c671c57b238f93ef3c297bc4a362
Ivan <ivanprats@hey.com>
2025-06-10 11:47:17 +0200

    improv: re-render the game iframe when payment succeeds
    As commented by Daniel, the game provider does not have the user's
    balance in sync in real time, so when it changes due to a payment: we
    need to re-launch the game iframe so the the user can see the updated
    balance.

--- app/components/game/frame/Iframe.vue
+++ app/components/game/frame/Iframe.vue
@@ -12,6 +12,8 @@ const props = defineProps({
   },
 });
 
+const iframeKey = ref(Date.now());
+
 onMounted(() => {
   $dependencies.common.asyncMessagePublisher.emit("frontend:events:games:game-session-started", {
     gameIdentifier: props.gameIdentifier,
@@ -23,10 +25,34 @@ onUnmounted(() => {
     gameIdentifier: props.gameIdentifier,
   });
 });
+
+/**
+ * Game providers are not in sync with User's wallet in real time,
+ * so we force a refresh of the game when a payment succeeds.
+ * Note: we do not use the wallet balance as it goes down with every bet, which the provider does keep track of.
+ */
+const paymentListenerId = ref<number | null>(null);
+onMounted(() => {
+  paymentListenerId.value = $dependencies.common.asyncMessagePublisher.subscribe(
+    "girobet-backend:events:payments:payment-status-updated",
+    async (event) => {
+      if (event.data.status === "succeeded") {
+        iframeKey.value = Date.now();
+      }
+    },
+  );
+});
+onUnmounted(() => {
+  if (paymentListenerId.value) {
+    $dependencies.common.asyncMessagePublisher.unsubscribe(paymentListenerId.value);
+    paymentListenerId.value = null;
+  }
+});
 </script>
 
 <template>
   <iframe
+    :key="iframeKey"
     :src="iFrameUrl"
     loading="eager"
     width="100%"
