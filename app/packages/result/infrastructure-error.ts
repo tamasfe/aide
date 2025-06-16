99d356ec27ae4002646cf1589794346bec6d1b57
Ivan <ivanprats@hey.com>
2025-06-16 13:06:03 +0200

    chore: use cause's error in InfrastructureError for better analysis and debugging

--- app/packages/result/infrastructure-error.ts
+++ app/packages/result/infrastructure-error.ts
@@ -6,17 +6,18 @@ export class InfrastructureError extends AbstractExtendedError {
 
   public static newFromError(metadata: Record<string, unknown>, error: Error | CustomError) {
     return new InfrastructureError(
-      "Unknown infrastructure error. More information in the cause",
+      error.message || "Unknown infrastructure error. More information in the cause",
       metadata,
       error,
     );
   }
 
   public static newFromUnknownError(metadata: Record<string, unknown>, unknownError: unknown) {
+    const error = InfrastructureError.parseCause(unknownError);
     return new InfrastructureError(
-      "Unknown infrastructure error. More information in the cause",
+      error.message || "Unknown infrastructure error. More information in the cause",
       metadata,
-      InfrastructureError.parseCause(unknownError),
+      error,
     );
   }
 }
