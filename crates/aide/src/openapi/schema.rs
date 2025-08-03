20a660eeccd7f88a2bb39d12a15cfc536eb3bae0
Jonas Platte <jplatte+git@posteo.de>
2025-08-03 23:14:34 +0200

    Add basic serialization tests for SchemaObject

--- crates/aide/src/openapi/schema.rs
+++ crates/aide/src/openapi/schema.rs
@@ -21,3 +21,46 @@ pub struct SchemaObject {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub example: Option<serde_json::Value>,
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::openapi::ExternalDocumentation;
+
+    use super::SchemaObject;
+    use schemars::Schema;
+    use serde_json::json;
+
+    #[test]
+    fn test_serialize_ref_schema() {
+        let serialized = serde_json::to_value(SchemaObject {
+            json_schema: Schema::new_ref("test".to_owned()),
+            external_docs: None,
+            example: None,
+        })
+        .unwrap();
+
+        assert_eq!(serialized, json!({ "$ref": "test" }));
+    }
+
+    #[test]
+    fn test_serialize_ref_schema_with_external_docs() {
+        let serialized = serde_json::to_value(SchemaObject {
+            json_schema: Schema::new_ref("test".to_owned()),
+            external_docs: Some(ExternalDocumentation {
+                description: None,
+                url: "http://example.org".to_owned(),
+                extensions: Default::default(),
+            }),
+            example: None,
+        })
+        .unwrap();
+
+        assert_eq!(
+            serialized,
+            json!({
+                "$ref": "test",
+                "externalDocs": { "url": "http://example.org" },
+            })
+        );
+    }
+}
