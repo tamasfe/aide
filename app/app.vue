54980dd3b29ac5f78a9ab0db546184bb53e01106
Ivan <ivanprats@hey.com>
2025-08-15 15:28:11 +0200

    improv: move console warn to vue hook for consistency

--- app/app.vue
+++ app/app.vue
@@ -4,10 +4,14 @@ const { t } = useI18n();
 // TODO: we need to show this console warning to users like facebook does.
 // we ideally should show it after 1-2 seconds so all the junk requests have time to
 // clear first, that way they actually see it. also.. it should go in a better spot
-setTimeout(() => {
-  consoleWarning(t("console_warning.title"), "danger");
-  consoleWarning(t("console_warning.description"));
-}, 600);
+onMounted(() => {
+  if (!import.meta.server) {
+    setTimeout(() => {
+      consoleWarning(t("console_warning.title"), "danger");
+      consoleWarning(t("console_warning.description"));
+    }, 600);
+  }
+});
 </script>
 
 <template>
