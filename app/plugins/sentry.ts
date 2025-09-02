1c5aca00cbf6711373562f06f910b26bace2a877
Ivan <ivanprats@hey.com>
2025-09-02 15:36:20 +0200

    improv: set user in Sentry for better exception tracking

---
+++ app/plugins/sentry.ts
@@ -0,0 +1,43 @@
+import * as Sentry from "@sentry/nuxt";
+import { InfrastructureError } from "~/packages/result/infrastructure-error";
+
+export default defineNuxtPlugin({
+  name: "sentry",
+  dependsOn: ["module-users-initiator"],
+  parallel: true,
+  async setup() {
+    const userStore = useUserStore();
+    const { $dependencies } = useNuxtApp();
+
+    if (userStore.user) {
+      try {
+        Sentry.setUser({
+          id: userStore.user.id.toString(),
+          email: userStore.user.email,
+          username: userStore.user.username,
+        });
+      }
+      catch (error) {
+        $dependencies.common.logger.error("Error setting Sentry user in plugin init", InfrastructureError.newFromUnknownError({ user: userStore.user }, error));
+      }
+    }
+
+    watch(() => userStore.user, (user) => {
+      try {
+        if (!user) {
+          Sentry.setUser(null);
+          return;
+        }
+
+        Sentry.setUser({
+          id: user.id.toString(),
+          email: user.email,
+          username: user.username,
+        });
+      }
+      catch (error) {
+        $dependencies.common.logger.error("Error setting Sentry user in userStore watch", InfrastructureError.newFromUnknownError({ user }, error));
+      }
+    });
+  },
+});
