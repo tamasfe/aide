793aaecf485381623be81886f91158c783af9740
Daniel Schneider <daniel.schneider@eramux.com>
2025-10-23 15:00:07 +0200

    feat: improve spinner

--- app/components/base/Spinner.vue
+++ app/components/base/Spinner.vue
@@ -1,15 +1,59 @@
-<script setup lang="ts">
-withDefaults(defineProps<{
-  size?: number;
-}>(), {
-  size: 24,
+<script lang="ts" setup>
+defineProps({
+  size: {
+    type: Number,
+    default: 24,
+  },
+  stroke: {
+    type: Number,
+    default: 3,
+  },
 });
 </script>
 
 <template>
-  <BaseIcon
-    :size="size"
-    name="tdesign:loading"
-    class="animate-spin"
-  />
+  <svg
+    :width="size"
+    :height="size"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g>
+      <circle
+        cx="12"
+        cy="12"
+        r="9.5"
+        fill="none"
+        :stroke-width="stroke"
+        stroke-linecap="round"
+      >
+        <animate
+          attributeName="stroke-dasharray"
+          dur="1.5s"
+          calcMode="spline"
+          values="0 150;42 150;42 150;42 150"
+          keyTimes="0;0.475;0.95;1"
+          keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="stroke-dashoffset"
+          dur="1.5s"
+          calcMode="spline"
+          values="0;-16;-59;-59"
+          keyTimes="0;0.475;0.95;1"
+          keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <animateTransform
+        attributeName="transform"
+        type="rotate"
+        dur="2s"
+        values="0 12 12;360 12 12"
+        repeatCount="indefinite"
+      />
+    </g>
+  </svg>
 </template>
